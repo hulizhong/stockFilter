89f3f24ad1b89f4cbb0d4fd9bc1288ef3b55418e
rabinhu <hulizhong@yeah.net>
2017-04-10 14:10:07 +0800

    add, can send email, not sms.

---
+++ KingMail.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import smtplib
+import chardet
+from email.mime.text import MIMEText
+from email.header import Header
+ 
+class KingMail(object):
+    """
+    email sent class replace sms notify. 
+    """
+    def __init__(self):
+        self.mailhost = "smtp.yeah.net"
+        self.user = "rabinhu@yeah.net"
+        self.pswd = "rabinhu007"
+        self.sender = 'rabinhu@yeah.net'
+        self.receivers = ['hulizhong@yeah.net', 'hulizhong@skyguard.com.cn']
+        self.smtpObj = smtplib.SMTP() 
+        self.smtpObj.connect(self.mailhost, 25)
+        self.smtpObj.login(self.user, self.pswd)  
+
+    def __del__(self):
+        ### bug, Exception smtplib.SMTPServerDisconnected: SMTPServerDisconnected('Connection unexpectedly closed',)
+        #self.smtpObj.quit()
+        pass
+
+    def sendtxt(self, content):
+        try:
+            ### text/plain will due to outlook recv mail as txt file.
+            message = MIMEText(content, 'plain', 'utf-8')
+            message['Subject'] = Header('金花策略中心', 'utf-8')
+            message['From'] = self.sender
+            ### bug, if receivers is a list, use .join(receivers)
+            #message['To'] = self.receivers
+            message['To'] = ','.join(self.receivers)
+            self.smtpObj.sendmail(self.sender, self.receivers, message.as_string())
+        except smtplib.SMTPException, e:
+            print "Error: 无法发送邮件, cause: ", e
+            #print isinstance(e.smtp_error, unicode)
+            #print isinstance(e.smtp_error, 'utf-8')
+            #print type(e)
+            #fencoding=chardet.detect(e.smtp_error)
+            #print fencoding
+
+    def sendhtml(self, content):
+        try:
+            ### text/plain will due to outlook recv mail as txt file.
+            message = MIMEText(content, 'html', 'utf-8')
+            message['Subject'] = Header('金花策略中心', 'utf-8')
+            message['From'] = self.sender
+            ### bug, if receivers is a list, use .join(receivers)
+            #message['To'] = self.receivers
+            message['To'] = ','.join(self.receivers)
+            self.smtpObj.sendmail(self.sender, self.receivers, message.as_string())
+        except smtplib.SMTPException, e:
+            print "Error: 无法发送邮件, cause: ", e
+            #print isinstance(e.smtp_error, unicode)
+            #print isinstance(e.smtp_error, 'utf-8')
+            #print type(e)
+            #fencoding=chardet.detect(e.smtp_error)
+            #print fencoding
+
+k = KingMail()
+fs = open("/home/stockFilter/dt")
+dt = fs.read()
+k.sendhtml(dt)
---
+++ fuQuang.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import sl4a
+droid = sl4a.Android()
+droid.smsSend("18501960037","sms")
+
+'''
+import lxml.etree  
+doc = lxml.etree.parse("/home/stockFilter/201612")
+
+import libxml2
+doc = libxml2.parseFile
+
+try: 
+    import xml.etree.cElementTree as ET 
+except ImportError: 
+    import xml.etree.ElementTree as ET 
+    import sys
+
+try:
+    tree = ET.parse("/home/stockFilter/201612")     #打开xml文档 
+    #root = ET.fromstring(country_string) #从字符串传递xml 
+    root = tree.getroot()         #获得root节点  
+    print root
+except Exception, e: 
+    print "Error:cannot parse file, cause ", e
+'''
+
+'''
+import xml.dom.minidom as domparse
+
+try:
+    dom = domparse.parse("/home/stockFilter/201612")  
+except Exception, e:
+    print "Will exit, cause ", e
+    raise e
+'''
+#root = dom.documentElement
+#print root
+#itemlist = root.getElementsByTagName('login')
+#item = itemlist[0]
+#print item.getAttribute("username")
+#print item.getAttribute("passwd")
+#
+#itemlist = root.getElementsByTagName("item")
+#item = itemlist[0]                   #通过在itemlist中的位置区分
+#print item.getAttribute("id") 
+#
+#item2 = itemlist[1]                  #通过在itemlist中的位置区分
+#print item2.getAttribute("id")
+
